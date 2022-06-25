3d07bba9952a0403cd06254f451b008e6b13fb28
jovannicatalan <catalanhurtadoj@guilford.edu>
2022-06-25 19:28:49 -0400

    ST 558 Project 1: R file that inlcudes render code to be run

---
+++ RenderCodeST558Proj1.R
@@ -0,0 +1,10 @@
+## R code to render RMarkdown file appropriately: ST 558 Project1
+rmarkdown::render('./_Rmd/ST558-Project1-Vignette.Rmd' ,
+                  output_format = 'github_document',
+                  output_file = "README",
+                  output_dir = './',
+                  output_options = list(
+                    toc = TRUE,
+                    toc_depth= 2,
+                    number_sections= TRUE)
+)
